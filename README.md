d371d95950f0da8e9407ce49ec0990f8c6eb49f4
yugmani <yugmani@yahoo.com>
2022-06-13 10:59:12 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-vykz94
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-vykz94)
---
+++ index.js
@@ -0,0 +1,22 @@
+// run `node index.js` in the terminal
+
+const fs = require('fs');
+
+// CREATING AND WRITING A FILE
+console.log('Creating and Writing a file');
+fs.writeFile('text1.txt', "Learning Node.js 'fs' core module", function (err) {
+  if (err) {
+    return console.error(err);
+  }
+
+  console.log('Data written successfully!');
+  console.log("Let's read newly written data");
+
+  //READING AN EXISTING FILE
+  fs.readFile('text1.txt', function (err, data) {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('Asynchronous read: ' + data.toString());
+  });
+});
